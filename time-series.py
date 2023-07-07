409f70ff974dd9ba06d6537c03c2d314cf190f03
grig <prowlergrig@yahoo.ie>
2023-07-07 11:32:55 +0300

    Revert "univariate data-data works!"
    This reverts commit 2dba33a01552e84ce5cb4f5a69232e017bc7f38c.

--- time-series.py
+++ time-series.py
@@ -1,41 +1,38 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.nn import functional as F
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+cuda = torch.cuda.is_available()
+if cuda:
+    device = torch.device('cuda')
+    print("using CUDA")
+else:
+    device = torch.device('cpu')
 
-# define a Transformer module for univariate time series prediction
-class UnivariateTransformer(nn.Module):
-    def __init__(self, d_model):
-        super(UnivariateTransformer, self).__init__()
-        self.linear = nn.Linear(d_model, 1)
+# define a Transformer in PyTorch and use it to predict a single value in a given sequence
 
-    def forward(self, src):
-        output = self.linear(src)
-        return output
-
-# create the model with appropriate input and output dimensions
-model = UnivariateTransformer(d_model=1).to(device)
-
+model = nn.Transformer(d_model=1, nhead=1)
+    
 # a toy univariate time series dataset
+
 data = torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]).unsqueeze(dim=-1).to(device)
 print(data)
 
 # training code
+
 def train():
-    model.train()
+    model.to(device).train()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     print("Training...")
-    for epoch in range(7000):
+    for epoch in range(10000):
         optimizer.zero_grad()
-        out = model(data)
+        out = model(data, data)
         loss = F.mse_loss(out, data)
         loss.backward()
         optimizer.step()
-        if epoch % 25 == 0:
-            print("Epoch", epoch, " - ", loss.item())
+        if epoch % 10 == 0:
+            print("Epoch", epoch, " - " , loss.item())
 
 train()
 
-print(model(data))
+print(model(data, data))
