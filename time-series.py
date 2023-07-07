1d678f4d6b0e7bec59d8f5723b04ecd5718d471c
grig <prowlergrig@yahoo.ie>
2023-07-07 11:19:23 +0300

    Update time-series.py

--- time-series.py
+++ time-series.py
@@ -15,7 +15,8 @@ model = nn.Transformer(d_model=1, nhead=1)
     
 # a toy univariate time series dataset
 
-data = torch.Tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).unsqueeze(dim=-1)
+data = torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]).unsqueeze(dim=-1).to(device)
+print(data)
 
 # training code
 
@@ -23,13 +24,14 @@ def train():
     model.to(device).train()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     print("Training...")
-    for epoch in range(100):
+    for epoch in range(10000):
         optimizer.zero_grad()
         out = model(data, data)
         loss = F.mse_loss(out, data)
         loss.backward()
         optimizer.step()
-        print(loss.item())
+        if epoch % 10 == 0:
+            print("Epoch", epoch, " - " , loss.item())
 
 train()
 
