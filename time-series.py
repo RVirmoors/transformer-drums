2dba33a01552e84ce5cb4f5a69232e017bc7f38c
grig <prowlergrig@yahoo.ie>
2023-07-07 11:25:33 +0300

    univariate data-data works!
    chatgpt trick - linear

--- time-series.py
+++ time-series.py
@@ -1,38 +1,41 @@
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
+import torch.nn.functional as F
 
-cuda = torch.cuda.is_available()
-if cuda:
-    device = torch.device('cuda')
-    print("using CUDA")
-else:
-    device = torch.device('cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
-# define a Transformer in PyTorch and use it to predict a single value in a given sequence
+# define a Transformer module for univariate time series prediction
+class UnivariateTransformer(nn.Module):
+    def __init__(self, d_model):
+        super(UnivariateTransformer, self).__init__()
+        self.linear = nn.Linear(d_model, 1)
 
-model = nn.Transformer(d_model=1, nhead=1)
-    
+    def forward(self, src):
+        output = self.linear(src)
+        return output
+
+# create the model with appropriate input and output dimensions
+model = UnivariateTransformer(d_model=1).to(device)
+
 # a toy univariate time series dataset
-
 data = torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]).unsqueeze(dim=-1).to(device)
 print(data)
 
 # training code
-
 def train():
-    model.to(device).train()
+    model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     print("Training...")
-    for epoch in range(10000):
+    for epoch in range(7000):
         optimizer.zero_grad()
-        out = model(data, data)
+        out = model(data)
         loss = F.mse_loss(out, data)
         loss.backward()
         optimizer.step()
-        if epoch % 10 == 0:
-            print("Epoch", epoch, " - " , loss.item())
+        if epoch % 25 == 0:
+            print("Epoch", epoch, " - ", loss.item())
 
 train()
 
-print(model(data, data))
+print(model(data))
