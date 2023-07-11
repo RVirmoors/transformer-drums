bfe50d6a6d3af9b2d7d18e15debfc8ec26638dd9
grig <prowlergrig@yahoo.ie>
2023-07-11 13:00:58 +0300

    full enc, partial dec. works

--- time-series.py
+++ time-series.py
@@ -18,6 +18,7 @@ pos_enc = "index" # "index" or "linear"
 class TransformerModel(nn.Module):
     def __init__(self, input_dim, output_dim, d_model, nhead):
         super(TransformerModel, self).__init__()
+        self.embedding = nn.Linear(input_dim, d_model)
         if pos_enc == "index":
             self.pos_enc = nn.Embedding(10, d_model)
         else:
@@ -25,11 +26,14 @@ class TransformerModel(nn.Module):
         self.transformer = nn.Transformer(d_model=d_model, nhead=nhead)
         self.fc = nn.Linear(d_model, output_dim)
 
-    def forward(self, src, tgt):
+    def forward(self, src, tgt):            
+        pos_src = torch.arange(0, len(src), dtype=torch.long).to(device)
+        pos_tgt = torch.round((tgt - 0.1)*10).long().squeeze()
+        src = self.embedding(src)
+        tgt = self.embedding(tgt)
         if pos_enc == "index":
-            pos = torch.arange(0, len(src), dtype=torch.long).to(device)#.unsqueeze(0)
-            src = src + self.pos_enc(pos)
-            tgt = tgt + self.pos_enc(pos)
+            src = src + self.pos_enc(pos_src)
+            tgt = tgt + self.pos_enc(pos_tgt)
         else:
             src = src + self.pos_enc(src)
             tgt = tgt + self.pos_enc(tgt)
@@ -42,8 +46,17 @@ model = TransformerModel(1, 1, d_model=8, nhead=2).to(device)
 # a toy univariate time series dataset
 
 data = torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]).unsqueeze(dim=-1).to(device)
-input_seq = data[:-1] 
-output_seq = data[1:]
+data = torch.stack([data, data, data, data, data, data, data]) # 7 batches
+
+input_seq = torch.Tensor([[0.1, 0.2, 0.3], 
+                          [0.2, 0.3, 0.4],
+                          [0.3, 0.4, 0.5],
+                          [0.4, 0.5, 0.6],
+                          [0.5, 0.6, 0.7],
+                          [0.6, 0.7, 0.8],
+                          [0.7, 0.8, 0.9]]).unsqueeze(dim=-1).to(device)
+
+output = torch.Tensor([0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]).unsqueeze(dim=-1).unsqueeze(dim=-1).to(device)
 
 #src_mask = model.generate_square_subsequent_mask(input_seq.size(0)).to(device)
 #tgt_mask = model.generate_square_subsequent_mask(output_seq.size(0)).to(device)
@@ -54,16 +67,17 @@ def train():
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
     print("Training...")
-    for epoch in range(12000):
-        optimizer.zero_grad()
-        out = model(data, data)
-        loss = F.mse_loss(out, data)
-        loss.backward()
-        optimizer.step()
-        if epoch % 25 == 0:
+    for epoch in range(2000):
+        for i in range(len(input_seq)):
+            optimizer.zero_grad()
+            out = model(data[i], input_seq[i])
+            loss = F.mse_loss(out[-1], output[i][0])
+            loss.backward()
+            optimizer.step()
+        if epoch % 10 == 0:
             print("Epoch", epoch, " - " , loss.item())
 
 train()
 
-print(model(data, data))
-print(model(1-data, 1-data))
+print("Last value is the usable prediction:", model(data[0], input_seq[0]))
+print(model(data[5], input_seq[5]))
