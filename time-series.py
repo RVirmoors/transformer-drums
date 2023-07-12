85aa7f3566728d905d1c20384fcbb34b1ac0a60f
grig <prowlergrig@yahoo.ie>
2023-07-12 11:31:52 +0300

    ts with sigmoid

--- time-series.py
+++ time-series.py
@@ -3,11 +3,15 @@
 # https://github.com/oliverguhr/transformer-time-series-prediction
 # https://github.com/AIStream-Peelout/flow-forecast
 
+pos_enc = "regular" # "regular" or "embedding" or "linLayer"
+train = True
+load_model = 'ckpt.pt'
 
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import math
+import os
 import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -26,23 +30,18 @@ data = torch.Tensor([[0., 0.1],
                     [0.5, 0.9],
                     [0.75, 1.0]]).to(device)
 
-data = torch.stack([data, data, data, data, data, data, data, data]) # 8 batches -> ENCODER
-# mask future
-for i in range(len(data)):
-    for j in range(len(data[i])):
-        if j > i+1:
-            data[i][j][1] = -np.inf
-#print(data)
-#exit()
+data = torch.stack([data, data, data, data, data, data, data, data, data]) # 9 batches -> ENCODER
 
-input_seq = torch.stack([data[0][i:i+2] for i in range(len(data[0])-2)]) # 8 batches, 2 timesteps -> DECODER
+input_seq = torch.stack([data[i][i] for i in range(len(data[0])-1)]).unsqueeze(dim=1) # 9 batches, 2 timesteps -> DECODER
+# print(input_seq)
+# exit()
 
-output = torch.Tensor([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]).unsqueeze(dim=-1).unsqueeze(dim=-1).to(device)
+output = torch.stack([data[i][i+1] for i in range(len(data[0])-1)]).unsqueeze(dim=1)
+# print(output)
+# exit()
 
 # define a Transformer in PyTorch and use it to predict a single value in a given time-series
 # data entries are pairs: [time, value]
-
-pos_enc = "regular" # "regular" or "embedding" or "linLayer"
 
 class TransformerModel(nn.Module):
     def __init__(self, input_dim, output_dim, d_model, nhead):
@@ -53,6 +52,7 @@ class TransformerModel(nn.Module):
         self.embedding = nn.Linear(input_dim, d_model)
         self.transformer = nn.Transformer(d_model=d_model, nhead=nhead)
         self.fc = nn.Linear(d_model, output_dim)
+        self.sigmoid = torch.nn.Sigmoid()
 
     def generatePE(self, x: torch.Tensor):
         if pos_enc == "embedding":
@@ -62,40 +62,64 @@ class TransformerModel(nn.Module):
         elif pos_enc == "regular":
             PE = torch.zeros((len(x), self.d_model))
             pos = x[:,0]
-            PE = torch.sin(pos * 2 * np.pi)
-            #PE = PE.repeat((2, self.d_model))
-            print(x, pos, PE)
+            PE = torch.sin(pos * 2 * np.pi).unsqueeze(-1)
+            PE = PE.repeat((1, self.d_model))
+            # print(PE)
             return PE
 
     def forward(self, src, tgt):
         src_posenc = self.generatePE(src)
-        src_tgtenc = self.generatePE(tgt)        
+        src_tgtenc = self.generatePE(tgt)
         src = self.embedding(src) + src_posenc
         tgt = self.embedding(tgt) + src_tgtenc
-        output = self.transformer(src, tgt)
+        # print(src)
+        src_mask = self.transformer.generate_square_subsequent_mask(src.size(0)).to(device)
+        # print(src_mask)
+        output = self.transformer(src, tgt, src_mask=src_mask)
+        # print(output)
         output = self.fc(output)
-        return output
+        return self.sigmoid(output)
     
 model = TransformerModel(2, 2, d_model=8, nhead=2).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
     
 # training code
 
 def train():
     model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
     print("Training...")
-    for epoch in range(8000):
+    for epoch in range(80):
         for i in range(len(input_seq)):
             optimizer.zero_grad()
             out = model(data[i], input_seq[i])
-            loss = F.mse_loss(out[-1], output[i][0])
+            # print((out, output[i]))
+            loss = F.mse_loss(out, output[i])
             loss.backward()
             optimizer.step()
         if epoch % 10 == 0:
             print("Epoch", epoch, " - " , loss.item())
 
-train()
+if load_model and not os.path.isfile(load_model):
+    print("No model found at", load_model, "-- starting from scratch...")
+    load_model = None
 
-print("Last value is the usable prediction:", model(data[0], input_seq[0]))
+if load_model:
+    print("Resuming training from", load_model, "...")
+    checkpoint = torch.load(load_model, map_location=device)
+    model.load_state_dict(checkpoint['model'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+    
+if train:
+    train()
+
+print(model(data[0], input_seq[0]))
 print(model(data[3], input_seq[3]))
 print(model(data[6], input_seq[6]))
+
+
+checkpoint = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            }
+torch.save(checkpoint, 'ckpt.pt')
