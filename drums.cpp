7e85e10b7b6917c1726905d64116cd46346f081a
grig <prowlergrig@yahoo.ie>
2023-07-18 10:44:58 +0300

    simple nn.module

--- drums.cpp
+++ drums.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <torch/torch.h>
 
+struct Net : torch::nn::Module {
+  Net(int64_t N, int64_t M)
+      : linear(register_module("linear", torch::nn::Linear(N, M))) {
+    another_bias = register_parameter("b", torch::randn(M));
+  }
+  torch::Tensor forward(torch::Tensor input) {
+    return linear(input) + another_bias;
+  }
+  torch::nn::Linear linear;
+  torch::Tensor another_bias;
+};
+
 int main() {
     torch::Device device = torch::kCPU;
     if (torch::cuda::is_available()) {
@@ -8,6 +20,9 @@ int main() {
     device = torch::kCUDA;
     }
 
-    torch::Tensor tensor = torch::eye(3).to(device);
-    std::cout << tensor << std::endl;
+    Net net(2, 3);
+    net.to(device);
+    for (const auto& p : net.parameters()) {
+        std::cout << p << std::endl;
+    }
 }
